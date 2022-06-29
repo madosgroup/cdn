1902e69e1b31ab17cda50c44e8866e89698cf536
Your Name <you@example.com>
2022-06-29 12:36:50 +0300

    first commit

---
+++ api/app/services/images/generateQr.py
@@ -0,0 +1,9 @@
+import os
+import qrcode
+from PIL import Image
+from pathlib import Path
+from ..helpers.uniqueFileName import generate_unique_name
+
+
+def qr_code_image(text = str, with_logo = bool):
+    pass
